7edee347792ad0c811be0670c096969c82cdf808
KanatoY <yamakawa_kanato@y-i-group.co.jp>
2026-05-07 06:02:35 +0900

    オブジェクト定義

--- blog-management-system/src/main/resources/db/create_blogs_table.sql
+++ blog-management-system/src/main/resources/db/create_blogs_table.sql
@@ -1,2 +1,11 @@
 DROP TABLE IF EXISTS blogs CASCADE;
+create table Blog {
+ id serial primary key
+ title varchar(255)  
+ content text
+ create_at timestamp default current_timestamp
+ updated_at timestamp default current_timestamp
+ deleted_at timestamp 
+}
+
 
