a439eab8d9d323a101662f953c8202b6fb0e228a
KanatoY <yamakawa_kanato@y-i-group.co.jp>
2026-06-17 23:19:18 +0900

    blog/detail-controller

--- blog-management-system/src/main/java/com/example/blog/controller/BlogController.java
+++ blog-management-system/src/main/java/com/example/blog/controller/BlogController.java
@@ -6,46 +6,45 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.blog.entity.Blog;
 import com.example.blog.form.BlogForm;
 import com.example.blog.service.BlogService;
 
-@Controller                    /**コントローラークラスであることを示すアノテーション*/
-public class BlogController {  /**ブログのリクエストを処理するクラス*/
+import lombok.RequiredArgsConstructor;
 
-    private final BlogService blogService;             /**ブログのビジネスロジックを担当するBlogService*/
+@Controller                    /** Springのコントローラークラスであることを示すアノテーション */
+@RequestMapping("/blogs")      /** このコントローラーが処理するリクエストのパスを指定するアノテーション */
+@RequiredArgsConstructor       /** Lombokのアノテーションで、finalなフィールドを持つクラスのコンストラクタを自動生成する */
+public class BlogController {  /** ブログのコントローラークラス */
 
-    public BlogController(BlogService blogService) {   /**BlogServiceをコンストラクタで注入する*/
-        this.blogService = blogService;                /**注入されたBlogServiceをフィールドにセットする*/
+    private final BlogService blogService;  /** ブログのビジネスロジックを担当するBlogService */
+
+    @GetMapping                                           /** HTTP GETリクエストを処理するメソッドであることを示すアノテーション */
+    public String index(Model model) {                    /** ブログの一覧を表示するメソッド */
+        List<Blog> blogs = blogService.list();            /** ブログの一覧を取得する */
+        model.addAttribute("blogs", blogs); /** モデルにブログの一覧を追加する */
+        return "blog/list";                                /** ブログの一覧を表示するビューの名前を返す */
     }
 
-    @GetMapping("/blogs")                    
-    public String index(Model model){
-        
-        List<Blog> blogs = blogService.list();                 /**BlogServiceのlistメソッドを呼び出してブログのリストを取得する*/
-        model.addAttribute("blogs", blogs);     /**取得したブログのリストをモデルに追加する*/
-        return "blog/list";                                   /**ブログのリストを表示するテンプレートの名前を返す*/
-   
+    @GetMapping("/new")                                       /** HTTP GETリクエストを処理するメソッドであることを示すアノテーション */
+    public String create(@ModelAttribute BlogForm blogForm) { /** ブログの作成フォームを表示するメソッド */
+        return "blog/form";                                   /** ブログの作成フォームを表示するビューの名前を返す */
     }
 
-    @GetMapping("/blogs/new")                                          /**新しいブログを作成するフォームを表示するメソッド*/
-    public String create(Model model) {                                /**モデルに新しいBlogFormを追加する*/
-       
-        model.addAttribute("blogForm", new BlogForm()); /**新しいBlogFormをモデルに追加する*/
-        return "blog/form";                                           /**ブログのフォームを表示するテンプレートの名前を返す*/
-    
+    @PostMapping                      
+    public String save(@ModelAttribute BlogForm blogForm) { /** ブログの作成フォームから送信されたデータを保存するメソッド */
+        blogService.create(blogForm);                       /** ブログの作成フォームから送信されたデータを保存する */
+        return "redirect:/blogs";                           /** ブログの一覧ページにリダイレクトする */
     }
 
-    @PostMapping("/blogs")                                               /**新しいブログを保存するメソッド*/
-    public String save(@ModelAttribute BlogForm blogForm, Model model) { /**BlogFormを引数として受け取る*/
-       
-        blogService.create(blogForm);                                    /**BlogServiceのcreateメソッドを呼び出して新しいブログを保存する*/
-        List<Blog> blogs = blogService.list();                           /**BlogServiceのlistメソッドを呼び出してブログのリストを取得する*/
-        model.addAttribute("blogs", blogs);                /**取得したブログのリストをモデルに追加する*/
-        return "blog/list";                                               /**ブログのリストを表示するテンプレートの名前を返す*/
-   
+    @GetMapping("/{id}")                                      
+    public String detail(@PathVariable int id, Model model) { /** ブログの詳細情報を表示するメソッド */ 
+        Blog blog = blogService.detail(id);                   /** ブログの詳細情報を取得する */
+        model.addAttribute("blog", blog);       /** モデルにブログの詳細情報を追加する */
+        return "blog/detail";                                 /** ブログの詳細情報を表示するビューの名前を返す */
     }
-
 }
