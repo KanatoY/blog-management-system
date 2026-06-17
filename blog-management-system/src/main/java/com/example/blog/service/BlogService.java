1e770dc2fdddab6c94caf86e578d6e363bef8d11
KanatoY <yamakawa_kanato@y-i-group.co.jp>
2026-06-17 23:18:25 +0900

    blog/detail-service

--- blog-management-system/src/main/java/com/example/blog/service/BlogService.java
+++ blog-management-system/src/main/java/com/example/blog/service/BlogService.java
@@ -1,32 +1,30 @@
 package com.example.blog.service;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.blog.entity.Blog;
 import com.example.blog.form.BlogForm;
 import com.example.blog.repository.BlogMapper;
+import lombok.RequiredArgsConstructor;
 
-@Service                             /**サービスクラスであることを示すアノテーション*/
-public class BlogService {           /**ブログのビジネスロジックを担当するクラス*/
+@Service                   /* Springのサービスクラスであることを示すアノテーション */
+@RequiredArgsConstructor   /* Lombokのアノテーションで、finalなフィールドを持つクラスのコンストラクタを自動生成する */
+public class BlogService { /* ブログのビジネスロジックを担当するサービスクラス */
 
-    @Autowired                       /**BlogMapperを自動で注入するアノテーション*/
-    private BlogMapper blogMapper;   /**ブログのデータアクセスを担当するBlogMapper*/
-    
-    public List<Blog> list() {       /**すべてのブログを取得するメソッド*/
-        return blogMapper.findAll(); /**BlogMapperのfindAllメソッドを呼び出してブログのリストを返す*/
+    private final BlogMapper blogMapper; /** ブログのデータアクセスを担当するBlogMapper */
+
+    public List<Blog> list() {           /* ブログの一覧を取得するメソッド */
+        return blogMapper.findAll();     /* ブログの一覧を取得する */
     }
 
-    
-    public void create(BlogForm blogForm) {      /**新しいブログを作成するメソッド*/
-          
-        Blog blog = new Blog();                  /**Blogエンティティの新しいインスタンスを作成する*/
-        blog.setTitle(blogForm.getTitle());      /**BlogFormからBlogエンティティにデータをコピーする*/
-        blog.setContent(blogForm.getContent());  /**BlogFormからBlogエンティティにデータをコピーする*/
-        blogMapper.save(blog);                   /**BlogMapperのsaveメソッドを呼び出してブログを保存する*/
+    public void create(BlogForm blogForm) {      /* ブログを作成するメソッド */
+        Blog blog = new Blog();                  /* 新しいブログのインスタンスを作成する */
+        blog.setTitle(blogForm.getTitle());      /* ブログのタイトルを設定する */
+        blog.setContent(blogForm.getContent());  /* ブログの内容を設定する */
+        blogMapper.save(blog);
+    }
 
+    public Blog detail(int id) {         /* ブログの詳細情報を取得するメソッド */
+        return blogMapper.findById(id);  /* ブログの詳細情報を取得する */
     }
 }
-
